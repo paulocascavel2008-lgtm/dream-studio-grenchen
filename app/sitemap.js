f38cb21bcb59dda0bff7c8d6d5250734a5bdcf65
paulocascavel2008-lgtm <paulocascavel2008@hotmail.com>
2026-03-19 17:50:52 +0100

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://dreamstudiogrenchen.ch',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://dreamstudiogrenchen.ch/portfolio',
+      lastModified: new Date(),
+    },
+  ];
+}
